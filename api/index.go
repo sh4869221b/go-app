895ea5ba8a788ac5acdb7d0d75b1f1ee39925b2f
sh4869 <sh4869221b@gmail.com>
2020-09-16 21:33:31 +0900

    initial commit

---
+++ api/index.go
@@ -0,0 +1,11 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Handler is function
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sh4869221b/go-app
+
+go 1.15
