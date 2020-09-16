eaa9c115bef0392276070715bff3b2e1bb983688
sh4869 <sh4869221b@gmail.com>
2020-09-16 21:47:50 +0900

    update

--- api/index.go
+++ api/index.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"io/ioutil"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -15,6 +16,25 @@ func hello2(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello World2")
 }
 
+func get(c echo.Context) error {
+	urls := []string{"https://280blocker.net/files/280blocker_adblock.txt", "https://raw.githubusercontent.com/nanj-adguard/nanj-filter/master/nanj-filter.txt", "https://blog-imgs-116-origin.fc2.com/b/t/o/btonews/5ch_matome_filter.txt", "https://raw.githubusercontent.com/tofukko/filter/master/Adblock_Plus_list.txt"}
+	return c.String(http.StatusOK, getUrls(urls))
+}
+
+func getUrls(urls []string) string {
+	var lists = ""
+	for _, url := range urls {
+		resp, err := http.Get(url)
+		if err != nil {
+			continue
+		}
+		defer resp.Body.Close()
+		body, _ := ioutil.ReadAll(resp.Body)
+		lists += string(body)
+	}
+	return lists
+}
+
 // Handler is router
 func Handler(w http.ResponseWriter, r *http.Request) {
 	e := echo.New()
@@ -22,6 +42,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	e.Use(middleware.Recover())
 	e.GET("/api/", hello)
 	e.GET("/api/2", hello2)
+	e.GET("/api/list.txt", get)
 
 	e.ServeHTTP(w, r)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sh4869221b/go-app
 
 go 1.15
+
+require github.com/labstack/echo/v4 v4.1.17
