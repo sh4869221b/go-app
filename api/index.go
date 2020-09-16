df3231fa8aef1094636f27f5cd73a6667693d9b1
sh4869 <sh4869221b@gmail.com>
2020-09-16 21:39:24 +0900

    add echo

--- api/index.go
+++ api/index.go
@@ -1,11 +1,27 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-// Handler is function
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello World")
+}
+
+func hello2(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello World2")
+}
+
+// Handler is router
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!!!!!</h1>")
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.GET("/api/", hello)
+	e.GET("/api/2", hello2)
+
+	e.ServeHTTP(w, r)
 }
