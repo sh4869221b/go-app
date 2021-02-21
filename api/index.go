435807e28bbc0722408812951f5cfb34dc0f1063
sh4869 <sh4869221b@gmail.com>
2021-02-22 00:16:48 +0900

    test

--- api/index.go
+++ api/index.go
@@ -14,7 +14,7 @@ func hello(c echo.Context) error {
 }
 
 func hello2(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello World2:"+time.Now().Format("200601"))
+	return c.String(http.StatusOK, "https://280blocker.net/files/280blocker_adblock_"+time.Now().Format("200601")+".txt")
 }
 
 // Handler is router
